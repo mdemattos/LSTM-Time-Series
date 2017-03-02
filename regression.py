e56d94eeb67e5fd8631107fc91a49b0de9e34ad0
Matheus de Mattos <m.matheus230@gmail.com>
2017-03-01 23:20:45 -0400

    comments debugging added.

--- regression.py
+++ regression.py
@@ -6,22 +6,27 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from matplotlib.lines import Line2D
+import datetime
 
 # fix random seed for reproducibility
 numpy.random.seed(7)
 
 # load the dataset
-dataframe = pandas.read_csv('datasets/monthly-water-usage-mlday-london.csv', usecols=[1], engine='python', skipfooter=3)
+dataframe = pandas.read_csv('datasets/monthly-reported-number-of-cases.csv', usecols=[1], engine='python', skipfooter=3)
 dataset = dataframe.values
 dataset = dataset.astype('float32')
+
+# non-normalized dataset
+originalDataset = dataset
 
 # normalize the dataset
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
 
 # split into train and test sets
-train_size = int(len(dataset) * 0.67)
+train_size = int(len(dataset) * 0.8)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
 print(len(train), len(test))
@@ -35,8 +40,11 @@ def create_dataset(dataset, look_back=1):
 	return numpy.array(dataX), numpy.array(dataY)
 
 # reshape into X=t and Y=t+1
+# number of previous time steps to use as input variables to predict the next time period — in this case defaulted to 1.
 look_back = 1
 trainX, trainY = create_dataset(train, look_back)
+
+# size of testX and testY is equal to len(dataset)-2
 testX, testY = create_dataset(test, look_back)
 
 # reshape input to be [samples, time steps, features]
@@ -48,21 +56,36 @@ model = Sequential()
 model.add(LSTM(4, input_dim=look_back))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, nb_epoch=20, batch_size=1, verbose=2)
+model.fit(trainX, trainY, nb_epoch=30, batch_size=1, verbose=2)
 
 # make predictions
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
+
 # invert predictions
 trainPredict = scaler.inverse_transform(trainPredict)
 trainY = scaler.inverse_transform([trainY])
 testPredict = scaler.inverse_transform(testPredict)
 testY = scaler.inverse_transform([testY])
-# calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-print('Test Score: %.2f RMSE' % (testScore))
+
+# calculate root mean absolute error
+trainScore = mean_absolute_error(trainY[0], trainPredict[:,0])
+print('Train Score: %.2f MAE' % (trainScore))
+testScore = mean_absolute_error(testY[0], testPredict[:,0])
+print('Test Score: %.2f MAE' % (testScore))
+
+print('-------------')
+print('Test data: \n', testY[0])
+print('Test predicted: \n', testPredict[:,0])
+print('----------------')
+
+# print('Valores reais: \n', originalDataset)
+# print('Valores previstos no treinamento:  \n', trainPredict)
+# print('Valores previstos no teste: \n', testPredict)
+
+# print("Tamanho do dataset", len(dataset))
+# print("Tamanho da base de treinamento", len(trainPredict))
+# print("Tamanho da base de testes", len(testPredict))
 
 # shift train predictions for plotting
 trainPredictPlot = numpy.empty_like(dataset)
@@ -73,7 +96,15 @@ testPredictPlot = numpy.empty_like(dataset)
 testPredictPlot[:, :] = numpy.nan
 testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
 # plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
+plt.xlabel('mês')
+plt.ylabel('Temperatura (em graus ºF)')
+
+plt.plot(scaler.inverse_transform(dataset), '-', label="Dados reais")
+plt.legend()
+
+# plt.plot(trainPredictPlot, '--', label="Predição treinamento")
+# plt.legend()
+
+plt.plot(testPredictPlot, ':', label="Predição testes")
+plt.legend()
 plt.show()
