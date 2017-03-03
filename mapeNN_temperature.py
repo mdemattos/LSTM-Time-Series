97f82e74c2f417e06086f604e4731326ca2a18ef
Matheus de Mattos Pereira <mattos.matheus@hotmail.com>
2017-03-03 13:36:03 -0400

    Update and rename maeNN_temperature.py to mapeNN_temperature.py

--- mapeNN_temperature.py
+++ mapeNN_temperature.py
@@ -56,5 +56,5 @@ for i in range (len(absoluteTemperatureErrorsNN)):
     sumCorrectErrors += absoluteTemperatureErrorsNN[i]
 
 
-print("MAE errado: ", sumWrongErrors/len(temperatureErrorsNN)) # 0.758
-print("MAE correto: ", sumCorrectErrors/len(absoluteTemperatureErrorsNN)) # 0.3128
+print("MAE errado: ", sumWrongErrors/len(temperatureErrorsNN)) # 0.075832823175 
+print("MAE correto: ", sumCorrectErrors/len(absoluteTemperatureErrorsNN)) # 0.312878941675 ARIMA é melhor
